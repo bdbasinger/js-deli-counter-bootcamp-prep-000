6b49e5c80179f0a6902970a1f93041f442b5f191
Brennan B <bdbasinger777@gmail.com>
2018-06-30 16:16:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var katzDeliLine = []
+
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   console.log(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
 
 
+a = 
