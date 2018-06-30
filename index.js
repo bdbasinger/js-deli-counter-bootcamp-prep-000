288db4d1ff136b0c91697ecf26063b0e0f8ec250
Brennan B <bdbasinger777@gmail.com>
2018-06-30 16:23:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    
+    console.log(`Currently serving ${katzDeliLine[0]}.`)
+    katzDeliLine.shift(katzDeliLine[0])
   }
 }
+
+// .shift(katzDeliLine[0])
